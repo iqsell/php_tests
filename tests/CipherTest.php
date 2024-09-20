9df9ea7048da22c359d98bef11b557dd6cf63d17
localhostj <91834688+iqsell@users.noreply.github.com>
2024-09-20 22:39:17 +0300

    Update CipherTest.php

--- tests/CipherTest.php
+++ tests/CipherTest.php
@@ -1,43 +1,44 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
-require_once __DIR__ . '/../src/Cipher.php';
+namespace Tests;
 
-// Тесты для функции cipher
+use PHPUnit\Framework\TestCase;
+use App\Cipher;
+
 class CipherTest extends TestCase
 {
     // Тест шифрования русского текста с положительным ключом
     public function testCipherWithRussianTextAndPositiveKey()
     {
-        $result = cipher('абвгд', 1);
+        $result = Cipher::cipher('абвгд', 1);
         $this->assertEquals('бвгде', $result);
     }
 
     // Тест шифрования английского текста с положительным ключом
     public function testCipherWithEnglishTextAndPositiveKey()
     {
-        $result = cipher('abcde', 2);
+        $result = Cipher::cipher('abcde', 2);
         $this->assertEquals('cdefg', $result);
     }
 
     // Тест шифрования с ключом, превышающим длину алфавита
     public function testCipherWithKeyGreaterThanAlphabetLength()
     {
-        $result = cipher('абвгд', 33); //
+        $result = Cipher::cipher('абвгд', 33); //
         $this->assertEquals('бвгде', $result);
     }
 
     // Тест шифрования текста с неалфавитными символами
     public function testCipherWithNonAlphabetCharacters()
     {
-        $result = cipher('абвгд123', 1);
+        $result = Cipher::cipher('абвгд123', 1);
         $this->assertEquals('бвгде123', $result);
     }
 
     // Тест шифрования текста с отрицательным ключом
     public function testCipherWithNegativeKey()
     {
-        $result = cipher('бвгде', -1);
+        $result = Cipher::cipher('бвгде', -1);
         $this->assertEquals('абвгд', $result);
     }
 }
