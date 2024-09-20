93746f2bc262586504b9b01b97ba847caaa21155
localhostj <91834688+iqsell@users.noreply.github.com>
2024-09-20 21:23:47 +0300

    Update CipherTest.php

--- tests/CipherTest.php
+++ tests/CipherTest.php
@@ -42,31 +42,3 @@ class CipherTest extends TestCase
     }
 }
 
-function cipher(string $text, int $key): string
-{
-    $russianAlphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя';
-    $russianAlphabetArray = preg_split('//u', $russianAlphabet, -1, PREG_SPLIT_NO_EMPTY);
-
-    $englishAlphabet = range('a', 'z');
-
-    $textChars = preg_split('//u', $text, -1, PREG_SPLIT_NO_EMPTY);
-
-    $result = '';
-    foreach ($textChars as $symbol) {
-        $currentAlphabet = $englishAlphabet;
-        if (in_array($symbol, $russianAlphabetArray)) {
-            $currentAlphabet = $russianAlphabetArray;
-        }
-        if (in_array($symbol, $currentAlphabet)) {
-            $currentIndex = array_search($symbol, $currentAlphabet);
-            $newIndex = ($currentIndex + $key) % count($currentAlphabet);
-            if ($newIndex < 0) {
-                $newIndex += count($currentAlphabet);
-            }
-            $result .= $currentAlphabet[$newIndex];
-        } else {
-            $result .= $symbol;
-        }
-    }
-    return $result;
-}
